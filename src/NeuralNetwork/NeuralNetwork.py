Corrected backpropagation error. Now working

src/NeuralNetwork/NeuralNetwork.py
@@ -34,6 +34,7 @@ class NeuralNetwork:
       self.bias = [None] + [np.full((self.layers[i],1),value) for i in range(1,len(self.layers))]
     
     self.activations = [np.zeros((self.layers[i],1)) for i in range(0,len(self.layers))]
+    self.weightedInputs = [None] + [np.zeros((self.layers[i],1)) for i in range(1,len(self.layers))]
     return self
 
   def loadOutputMap(self, outputMap):
@@ -45,7 +46,7 @@ class NeuralNetwork:
       sigma = 1/(1+np.exp(-x))
     elif self.activation == 'softmax':
       sigma = np.exp(x)/np.sum(np.exp(x))
-    return sigma if not prime else sigma-sigma**2
+    return sigma if not prime else sigma-np.square(sigma)
 
   def loadInput(self,input):
     if type(input) != np.ndarray or input.shape[1] != 1 or input.shape[0] != self.layers[0]:
@@ -59,5 +60,6 @@ class NeuralNetwork:
 
   def activate(self):
     for i in range(1,len(self.activations)):
-        self.activations[i] = self.activationFunction(np.dot(self.weights[i],self.activations[i-1])+self.bias[i])
+        self.weightedInputs[i] = np.dot(self.weights[i],self.activations[i-1])+self.bias[i]
+        self.activations[i] = self.activationFunction(self.weightedInputs[i])
     return self

src/NeuralNetwork/NeuralNetworkTrainer.py
@@ -55,9 +55,11 @@ class NeuralNetworkTrainer:
     input, output = self.vectorizeInputOuput(inputOutputDatapoint)
     self.network.loadInput(input)
     self.network.activate()
-    gradientToBias[-1] = np.multiply(self.costFunction(self.network.getOutput(),output,func,True),(self.network.getOutput()-np.square(self.network.getOutput())))
+    #gradientToBias[-1] = np.multiply(self.costFunction(self.network.getOutput(),output,func,True),(self.network.getOutput()-np.square(self.network.getOutput())))
+    gradientToBias[-1] = np.multiply(self.costFunction(self.network.getOutput(),output,func,True),self.network.activationFunction(self.network.weightedInputs[-1],True))
     for i in range(2,len(self.network.layers)):
-      gradientToBias[-i] = np.multiply(np.dot(self.network.weights[-i+1].transpose(),gradientToBias[-i+1]),(self.network.activations[-i]-np.square(self.network.activations[-i])))
+      #gradientToBias[-i] = np.multiply(np.dot(self.network.weights[-i+1].transpose(),gradientToBias[-i+1]),(self.network.activations[-i]-np.square(self.network.activations[-i])))
+      gradientToBias[-i] = np.multiply(np.dot(self.network.weights[-i+1].transpose(),gradientToBias[-i+1]),self.network.activationFunction(self.network.weightedInputs[-i],True))
     for i in range(1,len(self.network.layers)):
       gradientToWeights[i] = np.dot(gradientToBias[i],self.network.activations[i-1].transpose())
     return gradientToWeights, gradientToBias